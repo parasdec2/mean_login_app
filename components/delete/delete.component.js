93a7862d129cb87cc6c9224d6c3ff873fbea93d4
Paras Garg <49130632+parasdec2@users.noreply.github.com>
2021-09-28 23:22:01 +0530

    deleted a log statement

--- components/delete/delete.component.js
+++ components/delete/delete.component.js
@@ -2,7 +2,6 @@ export default {
   template:
     "<div><button type='submit' class='btn btn-primary'  ng-click='delete()'>Delete Accunt</button></div>",
   controller: function ($scope, $rootScope, dbProvider) {
-    console.log("in delete");
     $scope.delete = function () {
       dbProvider.deleteUser($rootScope.user._id);
     };
